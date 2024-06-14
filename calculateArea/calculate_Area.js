70342c1eaaa70a418f00c53b3dcc4bf91ba773f1
RoshawnL <roshawnlayne@gmail.com>
2024-06-14 10:35:30 -0400

    message

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,24 @@
+let length;
+let width;
+
+
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+
+    let area = length * width;
+
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+   }
+
+
+function calculateGrocerySum() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let sum = grocery1 + grocery2 + grocery3;
+    sum = sum.toFixed(2)
+
+    document.getElementById('cost').innerText = `The total price of the groceries are: ${sum}`;
+   }
