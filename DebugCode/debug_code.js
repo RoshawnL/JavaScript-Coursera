e8d42b9cd90ae0a05a34ebd87164a351f229dee8
RoshawnL <roshawnlayne@gmail.com>
2024-06-20 09:41:44 -0400

    added debug code

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,58 @@
+function performOperation() {
+// Get user input from input fields
+let num1 = parseInt(document.getElementById('input1').value);
+let num2 = parseInt(document.getElementById('input2').value);
+// Check if inputs are valid numbers
+if (!isNaN(num1) && !isNaN(num2)) {
+// Perform the operation
+                let result = multiply(num1, num2);
+                let dresult = divide(num1,num2)
+                let aresult = add(num1,num2)
+                let sresult = subtract(num1,num2)
+                // Display the result
+                displayResult(result);
+                displayResult(dresult);
+                displayResult(aresult);
+                displayResult(sresult);
+            } else {
+                displayResult('Please enter valid numbers');
+            }
+        }
+
+        function multiply(a, b) {
+            // Introduce a debugger statement to pause execution
+            debugger;
+
+            // Multiply the numbers
+            return a * b;
+        }
+
+        function divide(a, b) {
+            // Introduce a debugger statement to pause execution
+            debugger;
+
+            // Multiply the numbers
+            return a / b;
+        }
+
+        function add(a, b) {
+            // Introduce a debugger statement to pause execution
+            debugger;
+
+            // Multiply the numbers
+            return a + b;
+        }
+
+        function subtract(a, b) {
+            // Introduce a debugger statement to pause execution
+            debugger;
+
+            // Multiply the numbers
+            return a - b;
+        }
+
+        function displayResult(result) {
+            // Display the result in the paragraph element
+            const resultElement = document.getElementById('result');
+            resultElement.textContent = `The result is: ${result}`;
+        }
